1336a3e24c1c070fe607a6324817a7a05733cd36
Munna-k <40426953+Munna-k@users.noreply.github.com>
2018-09-03 16:13:33 +0530

    Update TestBase.java
    last change 3-9-2018

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -51,7 +51,7 @@ public class TestBase {
 		
 		
 		e_driver = new EventFiringWebDriver(driver);
-		// Now create object of EventListerHandler to register it with EventFiringWebDriver
+		
 		eventListener = new WebEventListener();
 		e_driver.register(eventListener);
 		driver = e_driver;
